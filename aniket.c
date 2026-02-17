5544ae3202ebabc84d16431aea83b41d89a58451
thakuraniket0804-create <thakuraniket0804@gmail.com>
2026-02-17 22:23:28 +0530

    aniket.c
    c.code

---
+++ aniket.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    float r,c,a,pi=3.14;
+    printf(" enter the radius");
+    scanf("%f"&r);
+    c=2*pi*r;
+    a=pi*r*r;
+    printf("\ncircumference of circle=%f" ,c);
+    printf("\narea of a circle=%f" ,a);
+
+    return 0;
+}
